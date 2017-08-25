f2afdc0fabc14dc504e878fe75380b648bdd3774
Sharan Srivatsa <sharans003@gmail.com>
2017-08-25 15:22:34 -0600

    added comments

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,9 +4,10 @@ import Interpreter.Interpreter;
 import Sentence.Sentence;
 import Utils.Utils;
 
-public class Tester {
-
+public class MainClass {
+	 /* If you prefer to read off a file provide the path here. If left empty a prompt will appear on the console.*/
 	public static String inputFilePath = "";
+	
 
 	public static void main(String[] args) {
 		List<String> inputStrings ;
@@ -18,23 +19,18 @@ public class Tester {
 		}
 		// Step 1 : Tokenize
 		List<Sentence> sentences = Sentence.convertTextToSentences(inputStrings);
-		Sentence.tokenize(sentences);
+		Sentence.tokenize(sentences); /* Implemented a tokenizer which tokenizes the entered sentence into custom tokens */
 		
 
 		//Step 2 : Remove WhiteSpaceTokens
 		for(int i =0; i<sentences.size();i++){
-			Sentence sentence = Sentence.removeWhiteSpaceTokens(sentences.get(i));
+			Sentence sentence = Sentence.removeWhiteSpaceTokens(sentences.get(i)); /* Done as a pre-processing step as this removes the need for rules for Whitespaces during evaluation */
 			sentences.set(i, sentence);
 		}
-//		for(Sentence s : sentences) {
-//			s.printTokens();
-//		}
+
 		//Step 3: Interpret validity of all sentences just based on operands
 		Interpreter interpreter = new Interpreter();
-		interpreter.evaluateString(sentences);		
-		
-		// Step 4: 
-		
+		interpreter.evaluateString(sentences); // Evaluate the correctness of the interpreter					
 	}
 
 
